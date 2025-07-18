ab1830a87d443e40d36969a19020929c739f39a8
Manjiri Chandure <manjirichandure@Manjiri-ka-MacBook-Pro.local>
2025-07-18 11:23:49 +0530

    Running server on Random assigned port having higher priority

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UserControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UserControllerIntegrationTest.java
@@ -3,20 +3,28 @@ package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //properties = {"server.port = 8081", "hostname=192.168.0.2"})
-@TestPropertySource(locations = "/application-test.properties",
-properties = "server.port=8888")
+//@TestPropertySource(locations = "/application-test.properties",
+//properties = "server.port=8888")
 public class UserControllerIntegrationTest {
 
     @Value("${server.port}")
     private int serverPort;
 
+//    Random_port have higher priority
+//    server port get 0
+    @LocalServerPort
+    private int tomcatServerPort;
+
     @Test
     void contextLoad(){
         System.out.println("Server Port " + serverPort);
-
+        System.out.println("tomcat Server Port" + tomcatServerPort);
     }
+
+
 }
