a043e64de3df7e5f8a265be1cbdb3a5f0ac22f71
Manjiri Chandure <manjirichandure@Manjiri-ka-MacBook-Pro.local>
2025-07-21 15:45:41 +0530

    Added test for method findUserById

--- src/test/java/com/appsdeveloperblog/tutorials/junit/io/UserRepositoryTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/io/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.appsdeveloperblog.tutorials.junit.io;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +19,22 @@ public class UserRepositoryTest {
     @Autowired
     UsersRepository usersRepository;
 
-    @DisplayName("testing findByEmail method of userRepository")
-    @Test
-    void testFindByEmailMethod_whenCorrectEmailProvided_thenReturnUserEntity(){
-//        Arrange
-        UserEntity userEntity = new UserEntity();
+    UserEntity userEntity;
+
+    @BeforeEach
+    void init(){
+        userEntity = new UserEntity();
         userEntity.setUserId(UUID.randomUUID().toString());
         userEntity.setFirstName("Manjiri");
         userEntity.setLastName("Chandure");
         userEntity.setEmail("chanduremanjiri@gmail.com");
         userEntity.setEncryptedPassword("12345678");
+    }
+
+    @DisplayName("testing findByEmail method of userRepository")
+    @Test
+    void testFindByEmailMethod_whenCorrectEmailProvided_thenReturnUserEntity(){
+//        Arrange
 
         testEntityManager.persistAndFlush(userEntity);
 //        Act
@@ -37,4 +44,18 @@ public class UserRepositoryTest {
         Assertions.assertEquals(userEntity.getEmail(), storedUser.getEmail(),
                 "Email should be equale");
     }
+
+    @DisplayName("Test findUserByIdMethod")
+    @Test
+    void testFindByUserIdMethod_whenValidUserIdProvided_thenReturnUserEntitty(){
+
+//        Arrange
+        testEntityManager.persistAndFlush(userEntity);
+//        Act
+        UserEntity user = usersRepository.findByUserId(userEntity.getUserId());
+//        Assert
+        Assertions.assertEquals(userEntity.getUserId(), user.getUserId(),
+                "UserId should be equale");
+    }
+
 }
